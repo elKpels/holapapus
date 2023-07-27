1695564d66988733026b07813f30d097d24260a6
elKpels <herrerasergio451@gmail.com>
2023-07-27 15:44:22 -0600

    xd.py

---
+++ xd.py
@@ -0,0 +1,20 @@
+def final():
+    print("Agarro mi mochila")
+    print("Me subo al carro")
+
+print("Me levanto")
+respuesta_banho = input("¿Me bañé?: 'Y/N' ")
+
+if respuesta_banho == "Y":
+    print("Me cambio")
+    respuesta_media_hora = input("¿Tengo más de media hora?: 'Y/N' ")
+    if respuesta_media_hora == "Y":
+        print("Desayuno")
+    final()
+else:
+    print("Me baño")
+    print("Me cambio")
+    respuesta_media_hora = input("¿Tengo más de media hora?: 'Y/N' ")
+    if respuesta_media_hora == "Y":
+        print("Desayuno")
+    final()
